6834b3d4d026130eca423c50c12eae89a5e41a05
jaehee21232 <a62533196@gmail.com>
2023-06-13 21:48:52 +0900

    feat: 커스텀 타일 add

---
+++ lib/todolist/common/component/custom_tile.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/todolist/common/model/todomodel.dart';
+
+class Custom_ListTile extends StatefulWidget {
+  final TodoModel todo;
+  final Function AddRef;
+  final Function RemoveRef;
+  const Custom_ListTile(
+      {super.key,
+      required this.todo,
+      required this.AddRef,
+      required this.RemoveRef});
+
+  @override
+  State<Custom_ListTile> createState() => _Custom_ListTileState();
+}
+
+class _Custom_ListTileState extends State<Custom_ListTile> {
+  bool select = false;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          select = !select;
+          if (select == true) {
+            widget.AddRef(widget.todo);
+          } else {
+            widget.RemoveRef(widget.todo);
+          }
+        });
+      },
+      child: Container(
+        decoration:
+            BoxDecoration(color: select ? Colors.blue[200] : Colors.white),
+        width: double.infinity,
+        height: 50,
+        child: Align(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              widget.todo.todo,
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          alignment: Alignment.centerLeft,
+        ),
+      ),
+    );
+  }
+}
