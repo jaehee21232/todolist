dae0a92e03fe53033e2736b5c01c011dfe7e99ed
jaehee21232 <a62533196@gmail.com>
2023-06-13 17:48:05 +0900

    feat: defaultlayout add

---
+++ lib/common/layout/default_layout.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class DefaultLayout extends StatelessWidget {
+  final Widget child;
+  final Color? backgroundColor;
+  final String? title;
+  final Widget? bottomNavigationBar;
+  const DefaultLayout(
+      {super.key,
+      required this.child,
+      this.backgroundColor,
+      this.title,
+      this.bottomNavigationBar});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: renderAppBar(),
+      body: child,
+      backgroundColor: backgroundColor ?? Colors.white,
+      bottomNavigationBar: bottomNavigationBar,
+    );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        title: Text(
+          title!,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        elevation: 1,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        centerTitle: true,
+      );
+    }
+  }
+}
