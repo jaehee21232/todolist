c22a63f9daca8f1fa7bd083547cc60c64aac5718
jaehee21232 <a62533196@gmail.com>
2023-06-14 19:21:24 +0900

    feat: date change add

--- lib/todolist/common/component/todo_detail_dialog.dart
+++ lib/todolist/common/component/todo_detail_dialog.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todolist/todolist/common/model/todomodel.dart';
+import 'package:todolist/todolist/common/riverpod/addtodo_provider.dart';
 import 'package:todolist/todolist/firebase/firestore.dart';
 
-class TodoDetailDialog extends StatelessWidget {
+class TodoDetailDialog extends ConsumerWidget {
   final TodoModel data;
   const TodoDetailDialog({super.key, required this.data});
 
   @override
-  Widget build(BuildContext context) {
-    TextEditingController TodoTextController =
+  Widget build(BuildContext context, WidgetRef ref) {
+    TextEditingController _TodoTextController =
         TextEditingController(text: data.todo);
+    DateTime _selectedDate = ref.watch(dateProvider);
+
     return AlertDialog(
       //모서리 둥글게
       shape: RoundedRectangleBorder(
@@ -18,11 +22,29 @@ class TodoDetailDialog extends StatelessWidget {
       title: Center(child: Text(data.todo)),
       content: Column(
         children: [
+          Text(
+              "${_selectedDate.year}-${_selectedDate.month}-${_selectedDate.day}"),
+          ElevatedButton(
+              onPressed: () {
+                //날짜 입력 받기
+                showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1900),
+                  lastDate: DateTime.now(),
+                ).then((selectedDate) {
+                  //date Riverpod으로 변경
+                  ref
+                      .read(dateProvider.notifier)
+                      .update((state) => selectedDate!);
+                });
+              },
+              child: Text("날짜 선택")),
           //todo 변경
           TextField(
-            controller: TodoTextController,
+            controller: _TodoTextController,
             onChanged: (value) {
-              print(TodoTextController.text);
+              print(_TodoTextController.text);
             },
           )
         ],
@@ -31,23 +53,23 @@ class TodoDetailDialog extends StatelessWidget {
       actions: [
         ElevatedButton(
             onPressed: () {
-              if (TodoTextController.text == "") {
+              if (_TodoTextController.text == "") {
                 return null;
               } else {
                 //model로 보내기 위해서 todo만 change된 값으로 다시 만들어서 보냄
                 final replacedata = TodoModel(
-                    date: data.date,
+                    date: _selectedDate.toString(),
                     id: data.id,
                     reference: data.reference,
                     success: data.success,
-                    todo: TodoTextController.text);
+                    todo: _TodoTextController.text);
                 DataReplace(replacedata);
                 Navigator.pop(context);
               }
             },
             child: Text("완료")),
         ElevatedButton(
-            autofocus: TodoTextController.text == "" ? false : true,
+            autofocus: _TodoTextController.text == "" ? false : true,
             onPressed: () {
               DataRemove(data);
               Navigator.pop(context);
